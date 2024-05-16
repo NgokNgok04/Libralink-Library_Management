a5e9e78db561f87f9c306e44b4822b0c59029f15
unknown <117185466+NgokNgok04@users.noreply.github.com>
2024-05-16 21:50:57 +0700

    feat: test init

--- src/app/testajabro.py
+++ src/app/testajabro.py
@@ -1,22 +1,7 @@
-from PySide6.QtCore import QRect
-from PySide6.QtGui import QColor
-from PySide6.QtWidgets import QApplication, QGraphicsDropShadowEffect, QWidget, QVBoxLayout, QPushButton
+# content of test_sample.py
+def func(x):
+    return x + 1
 
-app = QApplication([])
 
-window = QWidget()
-window.setGeometry(QRect(100, 100, 400, 300))  # Set the size and position of the window
-window.setStyleSheet(u"background-color: white;")
-layout = QVBoxLayout(window)
-button = QPushButton("Click Me")
-layout.addWidget(button)
-
-shadow = QGraphicsDropShadowEffect()
-shadow.setBlurRadius(20)
-shadow.setColor(QColor(0, 0, 0, 160))  # Semi-transparent black
-shadow.setOffset(10, 10)  # Offset for X and Y
-
-button.setGraphicsEffect(shadow)
-
-window.show()
-app.exec()
+def test_answer():
+    assert func(3) == 5
